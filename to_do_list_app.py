c9d12ae0ff372ed5fb866f47a4f395cc5df71ee1
Dzhamshitov Danial <finmotive7@gmail.com>
2026-03-25 23:41:44 +0500

    FIXED see_list func

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -12,9 +12,9 @@ def creating_task_user():
     }
     user_input_task_topic=input('write your task TOPIC-')
     user_input_task_decription=input('write your task description\nwhat you should do-')
-    task_list['topic']=user_input_task_topic
+    task_list['TOPIC']=user_input_task_topic
     task_list['description']=user_input_task_decription
-    task_list['date']=now.strftime("%Y-%m-%d %H:%M:%S")
+    task_list['ADDED_DATE']=now.strftime("%Y-%m-%d %H:%M:%S")
     return task_list
 def create_task(file_name):
     task=creating_task_user()
@@ -36,10 +36,12 @@ def clear_list(file_name):
         json.dump({},f)
         print('LIST IS CLEAR')
 def see_list(file_name):
-    print(json.dumps(file_name,indent=2))
+    with open(file_name,'r') as f:
+        read_file=json.load(f)
+    print(json.dumps(read_file,indent=1))
 file_name='tasks_list_data.json'
 print('Hello its a todolist app at python')
-print('Enter what you want to do\n 1.Create a new task \n 2.Delete a Task \n 3.See all tasks \n 4.Clear whole list ')
+print('Enter what you want to do\n 1.Create a new task \n 2.Mark a Task (Done) \n 3.See all tasks \n 4.Clear whole list ')
 user_input=int(input())
 if user_input==1:
     create_task(file_name)
